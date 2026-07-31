2d903c6e314f83a01fa8a7d8c24581cd0a91e5f4
riteshkrpatel8960 <riteshkrpatel8960@gmail.com>
2026-07-31 17:42:25 +0530

    Update forgot and reset password

--- db.js
+++ db.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+    });
+
     console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection failed", error);
-    process.exit(1);
+    console.error("❌ MongoDB connection failed");
+    console.error(error.message);
   }
 };
 
