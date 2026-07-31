fbc39ebb517980fac0f61c3a7fb95e33c5ed1496
riteshkrpatel8960 <riteshkrpatel8960@gmail.com>
2026-07-31 11:33:16 +0530

    Updated mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,17 +2,23 @@ import nodemailer from "nodemailer";
 
 export const sendOTPEmail = async (to, otp) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
+      pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
   });
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to,
     subject: "StudyAI Password Reset OTP",
-    text: `Your OTP is ${otp}. Valid for 5 minutes.`
+    text: `Your OTP is ${otp}. Valid for 5 minutes.`,
   });
 };
