35206c4a96d3a6585086fc4bc5ea266078a4ec66
riteshkrpatel8960 <riteshkrpatel8960@gmail.com>
2026-08-02 20:02:56 +0530

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,24 +1,26 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 export const sendOTPEmail = async (to, otp) => {
-  const { data, error } = await resend.emails.send({
-    from: "StudyAI <onboarding@resend.dev>",
-    to: [to],
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  await transporter.sendMail({
+    from: `"StudyAI" <${process.env.EMAIL_USER}>`,
+    to,
     subject: "StudyAI Password Reset OTP",
     html: `
       <h2>StudyAI Password Reset</h2>
       <p>Your OTP is:</p>
       <h1>${otp}</h1>
       <p>This OTP is valid for 5 minutes.</p>
-    `,
+    `
   });
 
-  if (error) {
-    console.error(error);
-    throw new Error(error.message);
-  }
-
-  console.log("✅ Email Sent", data);
+  console.log("✅ OTP Email Sent");
 };
